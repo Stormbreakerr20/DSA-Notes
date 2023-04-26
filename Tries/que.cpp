c43347a4323747132cbf294cdded3e72c9091436
Luv Sharma <luvsharma.del@gmail.com>
2023-04-26 19:11:16 +0530

    new

---
+++ Tries/que.cpp
@@ -0,0 +1,5 @@
+//  - Longest Common Prefix: https://www.codingninjas.com/codestudio/problems/longest-common-prefix_2090383?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
+
+// imp : for search results in google
+
+// phone directoy :  https://www.codingninjas.com/codestudio/problems/implement-a-phone-directory_1062666?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0
--- arrayandque.cpp/string.cpp
+++ arrayandque.cpp/string.cpp
@@ -30,10 +30,6 @@ int main(){
         else if  ((s[i]>='a'&&s[i]<='z')|| ( s[i]>=0&&s[i]<=9)){
             temp.push_back(s[i]);
         }
-
-   
-
-
     }
     cout<<endl<<palindrome(temp);
 
